8cdaaff0e694981d7497619b8680fa518156b7e1
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2024-09-13 16:26:28 +0300

    tested packages added

--- app.ts
+++ app.ts
@@ -1,12 +1,1 @@
-import fs from 'fs';
 
-
-const name = fs.readFileSync('name.txt', 'utf-8');
-console.log(name);
-
-fs.unlink('name.txt', (err) => {
-    if (err) throw err;
-
-    console.log('file deleted');
-});
- 
